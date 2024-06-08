875f0bec9e6da21bf9b0273d505fbe0489436092
Suhas A <suhasamaresh@gmail.com>
2024-06-08 16:13:28 +0530

    "first commit"

---
+++ app/form/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <input></input>
+  )
+}
+
+export default page
---
+++ components/navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>this is the navbar</div>
+  )
+}
+
+export default navbar
